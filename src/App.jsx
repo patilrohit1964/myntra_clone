35c57f7790aa38b1e952d7459c743eb04a3d7b5d
patilrohit1964 <patilrohit19649@gmail.com>
2024-08-22 11:17:45 +0530

    added backend url

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import './App.css'
 import Navbarnav from './components/Navbarnav'
 import AllRoutes from './routes/AllRoutes'
-// import Login from './pages/Login'
 import Footer from './components/Footer'
 
 function App() {
